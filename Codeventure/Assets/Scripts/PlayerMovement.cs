9c9e36ed204d990b439a2c38fbb7c1bf961b5733
JixS4v <jixs4v@gmail.com>
2020-05-16 16:05:37 +0200

    Created project and basic movement

---
+++ Codeventure/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	//the speed of the movement
+	public float speed = 10f;
+
+	//rigidbody: this controls all the physics
+	private Rigidbody2D rb;
+	//input variables
+	private float hor;
+	private float ver;
+	//the velocity after parsing input
+	private Vector3 newVel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		//getting the component from the gameobject. It's like getting a child node in godot
+		rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//grab all inputs
+		hor = Input.GetAxis("Horizontal");
+		ver = Input.GetAxis("Vertical");
+
+		//setting the new velocity that is the inputs multiplied by speed
+		newVel = new Vector2(hor, ver) * speed;
+
+		//applying the new velocity
+		rb.velocity = newVel;
+    }
+}
