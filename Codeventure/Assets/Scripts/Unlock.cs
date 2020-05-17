f4de071e8b62dd7fcd6be48b9af289ab25f2407b
Roombabatata <fares.setbels@gmail.com>
2020-05-17 23:42:47 +0100

    Buged Door and Button

---
+++ Codeventure/Assets/Scripts/Unlock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unlock : MonoBehaviour
+{
+    public GameObject door;
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Hello World!");
+        door.GetComponent<Door>().Open();
+    }
+}
