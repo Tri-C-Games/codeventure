ed701169f0444246cce451ce3d26f070cf402587
Roombabatata <fares.setbels@gmail.com>
2020-05-18 11:42:37 +0100

    Add the door

--- Codeventure/Assets/Scripts/Door.cs
+++ Codeventure/Assets/Scripts/Door.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour {
+    
     public void Open()
     {
-        var transformPosition = transform.position;
-        transformPosition.y += 100;
+        transform.position = new Vector3(transform.position.x, 5f, transform.position.z); //the door is opening
+    }
+
+    public void Close()
+    {
+        transform.position = new Vector3(transform.position.x, 1.713078f, transform.position.z); // the door is closing
     }
 }
