73fd131793a42c67732f9c0008416f0452616abf
mariothedog <mariothedogmc@gmail.com>
2020-05-20 23:38:07 +0100

    Added a button [WIP] + Door + 2D Extras plugin

---
+++ Codeventure/Assets/Scripts/Objects/Button.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject door;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Button Pressed.");
+        door.GetComponent<Door>().open();
+    }
+}
