e97b7f99f1da4c25f7b5672226fc4e7f0389ea97
mariothedog <mariothedogmc@gmail.com>
2020-05-20 20:38:37 +0100

    Update PlayerMovement.cs

--- Codeventure/Assets/Scripts/Player/PlayerMovement.cs
+++ Codeventure/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,39 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	//the speed of the movement
 	public float speed = 10f;
 
-	//rigidbody: this controls all the physics
 	private Rigidbody2D rb;
-	//input variables
+
+	// Input variables
 	private float hor;
 	private float ver;
-	//the velocity after parsing input
+
+	// The velocity after parsing input
 	private Vector3 newVel;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-		//getting the component from the gameobject. It's like getting a child node in godot
 		rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-		//grab all inputs
+		// Grab all inputs
 		hor = Input.GetAxis("Horizontal");
 		ver = Input.GetAxis("Vertical");
 
-		//setting the new velocity that is the inputs multiplied by speed
+		// Setting the new velocity that is the inputs multiplied by speed
 		// The movement vector is clamped as to keep the speed the same regardless of if the player is travelling diagonally or not
 		newVel = Vector2.ClampMagnitude(new Vector2(hor, ver), 1) * speed;
 
-		//applying the new velocity
+		// Applying the new velocity
 		rb.velocity = newVel;
     }
 }
