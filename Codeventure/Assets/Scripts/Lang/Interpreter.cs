b45bce36f4aea654d2d2384fb5178c706c9e4b95
JixS4v <jixs4v@gmail.com>
2020-05-20 15:12:20 +0200

    @everyone new datatype support

--- Codeventure/Assets/Scripts/Lang/Interpreter.cs
+++ Codeventure/Assets/Scripts/Lang/Interpreter.cs
@@ -3,18 +3,19 @@ using System.Text.RegularExpressions;
 using UnityEngine;
 using System.IO;
 using System.Globalization;
+using System.Linq;
 
 public class Interpreter:MonoBehaviour
 {
 	CultureInfo ci = CultureInfo.InvariantCulture;
-	public Dictionary<string, float> variables;
+	public Dictionary<string, dynamic> variables;
 	private string code;
 	private string[] lines;
 	string fileName = @"Assets\Scripts\Lang\main.owo";
 
 	void Start()
 	{
-		variables = new Dictionary<string, float>();
+		variables = new Dictionary<string, dynamic>();
 		code = File.ReadAllText(fileName);
 			lines = SeparateLines(code);
 			foreach (var line in lines)
@@ -26,7 +27,10 @@ public class Interpreter:MonoBehaviour
 			}
 		}
 
-
+	void PRINT(string text)
+	{
+		Debug.Log(text);
+	}
 		string[] SeparateLines(string _code)
 		{
 			string[] _lines = Regex.Split(_code, ";");
@@ -46,16 +50,46 @@ public class Interpreter:MonoBehaviour
 				string[] _split = Regex.Split(_line,"=");
 				_key = _split[0];
 				_value = _split[1];
-				Debug.Log(float.Parse(_value));
-				
-				if (variables.ContainsKey(_key))
-				{
-					variables[_key] = float.Parse(_value, ci);
-				}
-				else
-				{
-					variables.Add(_key, float.Parse(_value, ci));
-				}
+				//TODO: Parse expressions
+				Var(_key,_value);
 			}
+			else if (_line.ToLower().IndexOf('(') != -1&& _line.ToLower().IndexOf(')') != -1)
+			{
+				string _func;
+				string _args;
+				string[] _split = Regex.Split(_line, "(");
+				_func = _split[0];
+				_args = _split[1];
+				_args = Regex.Replace(_args, ")", string.Empty);
+				Debug.Log(float.Parse(_args));
+				
+			}
+	}
+	void Var(string _key, string _value)
+	{
+		dynamic parsedValue;
+		if (_value.TakeWhile(c => c == '"').Count() > 2)
+		{ 
+			_value = _value.Trim('"');
+			parsedValue = _value;
 		}
+			double _result;
+		else if (double.TryParse(_value,out _result)){
+			parsedValue = _result;
+		}
+		else
+		{
+			return; //TODO: Error handling
+		}
+		
+
+		if (variables.ContainsKey(_key))
+		{
+			variables[_key] = parsedValue;
+		}
+		else
+		{
+			variables.Add(_key, parsedValue);
+		}
+	}
 }
