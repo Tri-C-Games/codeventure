a1c68fef67bdce963a09a4b33ca1f8aab5a4b4a7
Roombabatata <fares.setbels@gmail.com>
2020-05-21 16:59:59 +0100

    The turret now follows the player

---
+++ Codeventure/Assets/TurretScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour {
+  public Transform target;
+
+    void Update() {
+      Quaternion rotation = Quaternion.LookRotation
+          (target.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+      transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+    }
+}
