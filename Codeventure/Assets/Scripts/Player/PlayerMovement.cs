f4567f05def8fe5b77d3063cac1479cdcd286f4b
Roombabatata <fares.setbels@gmail.com>
2020-05-21 16:12:06 +0100

    Add the box prefab

--- Codeventure/Assets/Scripts/Player/PlayerMovement.cs
+++ Codeventure/Assets/Scripts/Player/PlayerMovement.cs
@@ -33,4 +33,9 @@ public class PlayerMovement : MonoBehaviour
         // Applying the new velocity
         rb.velocity = newVel;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.isTrigger = true;
+    }
 }
