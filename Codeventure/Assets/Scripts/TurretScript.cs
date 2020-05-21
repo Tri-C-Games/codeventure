2400b4419a953bf722b0b9d9e3d8053a3539ddb1
Roombabatata <fares.setbels@gmail.com>
2020-05-21 22:16:49 +0100

    Add the turret Prefab

--- Codeventure/Assets/Scripts/TurretScript.cs
+++ Codeventure/Assets/Scripts/TurretScript.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class TurretScript : MonoBehaviour {
   public Transform target;
+  public Transform firePoint;
+  public GameObject bullet;
 
     void Update() {
+      RaycastHit2D hit = Physics2D.Raycast(firePoint.position, firePoint.right);
       Quaternion rotation = Quaternion.LookRotation
           (target.transform.position - transform.position, transform.TransformDirection(Vector3.up));
       transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+
+      if(hit) {
+        if(hit.collider.name == "Player"){
+          Shoote();
+        }}
+      }
+      public void Shoote() {
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+      }
     }
-}
