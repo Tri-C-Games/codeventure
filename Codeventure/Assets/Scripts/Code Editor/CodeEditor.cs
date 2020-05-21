4bf7218ea1b15faae1f7858f03780fdda760f358
mariothedog <mariothedogmc@gmail.com>
2020-05-21 12:50:59 +0100

    Code editor (Very WIP).

---
+++ Codeventure/Assets/Scripts/Code Editor/CodeEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CodeEditor : MonoBehaviour
+{
+    public static bool gamePaused = false;
+
+    public GameObject codeEditor;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        codeEditor.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    private void Resume()
+    {
+        codeEditor.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+}
