6f74ec10f968c1ba2c9f2d692b12a2652211886a
Roombabatata <fares.setbels@gmail.com>
2020-05-24 16:10:47 +0100

    First version of the laser

---
+++ Codeventure/Assets/Scripts/LaserScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour {
+  public Transform target;
+  public Transform firePoint;
+  public GameObject bullet;
+  public AudioSource fireSound;
+  public LayerMask detectPlayerMask;
+  public LineRenderer lr;
+  private const float range = 100;
+
+    void Update() {
+      transform.right = target.transform.position - transform.position;
+
+      RaycastHit2D foundPlayer = Physics2D.Raycast(firePoint.position, firePoint.right, range, detectPlayerMask);
+
+      if (foundPlayer) {
+          Shoot();
+      }
+    }
+    private void Shoot() {
+
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+        fireSound.Play();
+    }
+}
